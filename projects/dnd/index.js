e638bafc4e6d46dca300d96d8cb2989c288b10b3
amprihotko <amprihotko@itertech.by>
2021-08-10 20:31:28 +0300

    week 4 - dnd

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -21,11 +21,39 @@ const homeworkContainer = document.querySelector('#app');
 
 document.addEventListener('mousemove', (e) => {});
 
-export function createDiv() {}
+const generateColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+export function createDiv() {
+  const div = document.createElement('div');
+  const divW = getRandomInt(250);
+  const divH = getRandomInt(250);
+  div.classList.add('draggable-div');
+  div.style.backgroundColor = generateColor();
+
+  div.style.width = divW + 'px';
+  div.style.height = divH + 'px';
+  div.style.top = getRandomInt(document.documentElement.clientHeight - divH) + 'px';
+  div.style.left = getRandomInt(document.documentElement.clientWidth - divW) + 'px';
+  div.draggable = true;
+  return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
 addDivButton.addEventListener('click', function () {
   const div = createDiv();
   homeworkContainer.appendChild(div);
+});
+
+let layerY, dragged, layerX;
+homeworkContainer.addEventListener('dragstart', (event) => {
+  dragged = event.target;
+  layerX = event.layerX;
+  layerY = event.layerY;
+});
+
+homeworkContainer.addEventListener('dragend', (event) => {
+  dragged.style.top = event.clientY - layerY + 'px';
+  dragged.style.left = event.clientX - layerX + 'px';
 });
